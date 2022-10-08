e3ac9617ea4c4e5de6fba39f5f9b8a5bc1e19e4f
Romário Ferreira <romarioferreirarezende@hotmail.com>
2022-10-08 18:44:39 -0300

    implemented insertReview

---
+++ src/main/java/com/devsuperior/movieflix/services/reviewService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.movieflix.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+
+@Service
+public class reviewService {
+
+	@Autowired
+	private ReviewRepository repository;
+
+	@Autowired
+	private MovieRepository movieRepository;
+
+	@Transactional
+	public ReviewDTO insert(ReviewDTO dto) {
+
+		Review review = new Review();
+		review.setText(dto.getText());
+
+		Movie movie = movieRepository.getOne(dto.getMovieId());
+		review.setMovie(movie);
+
+		review = repository.save(review);
+
+		return new ReviewDTO(review);
+
+	}
+
+}
